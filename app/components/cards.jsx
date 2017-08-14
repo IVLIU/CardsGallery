f4403d7c9dc19d4f29a1e9b4ccc38715242a3af8
IVLIU <liupyliupy@outlook.com>
2017-08-14 21:15:31 +0800

    haha

--- app/components/cards.jsx
+++ app/components/cards.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import CSSModules from 'react-css-modules';
 import styles from '../styles/card.css';
 class Cards extends React.Component{
+    static defaultProps={
+        url:'',
+        title:''
+    }
     render(){
+        const {url,title}=this.props;
         return (
-            <div>
-                <img src={require('../images/Hedgehog.png')} />
+            <div className={styles.card}>
+                <div className={styles.card_style}>
+                    <img className={styles.img} src={url} /><br />
+                    <h2 className={styles.title}>{title}</h2>
+                </div>
             </div>
         )
     }
@@ -13,7 +21,9 @@ class Cards extends React.Component{
 
 const App=()=>(
     <div className={styles.container}>
-        <Cards />
+        <Cards url={require('../images/buck.png')} title='BUCK' />
+        <Cards url={require('../images/Hedgehog.png')} title='HEDGEHOG' />
+        <Cards url={require('../images/Hippo.png')} title='HIPPO' />
     </div>
 )
 
