e8ac955d31862e921f318881d753e49931f8e8e9
IVLIU <liupyliupy@outlook.com>
2017-09-09 11:27:42 +0800

    restart

--- app/main.js
+++ app/main.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'react-addons';
-import App from './components/cards.jsx';
-import getData from './scripts/fetch.js';
-let gen=getData('./sourceData/data.json')();
-let fetchBack=gen.next();
-let list=gen.next();
-console.log(fetchBack);
-console.log(list);
-ReactDOM.render((
-    <App />
-),document.getElementById('content'));
+
