379d37cb22520a7daf7de2a579bd267cbed63c80
IVLIU <liupyliupy@outlook.com>
2017-09-09 11:25:58 +0800

    restart

--- app/main.js
+++ app/main.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import 'react-addons';
 import App from './components/cards.jsx';
 import getData from './scripts/fetch.js';
-let gen=getData('./sourceData/data.json');
-// let arrPromise=gen.next().value;
-// console.info('firstNext',arrPromise);
-// let newArr=gen.next(arrPromise);
-// console.log('secondNext',newArr);
+let gen=getData('./sourceData/data.json')();
+let fetchBack=gen.next();
+let list=gen.next();
+console.log(fetchBack);
+console.log(list);
 ReactDOM.render((
     <App />
 ),document.getElementById('content'));
